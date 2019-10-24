363f80fb721abf827b36f69bed651c7cbed8e4c5
Selena Yang <syang@lcs.on.ca>
2019-10-24 14:42:34 -0400

    connect the view controller of triangle and added outlet

---
+++ AreaHelper/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Yang, Selena on 2019/10/16.
+//  Copyright © 2019 Yang Selena. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
+
---
+++ AreaHelper/TriangleViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Yang, Selena on 2019/10/16.
+//  Copyright © 2019 Yang Selena. All rights reserved.
+//
+
+import UIKit
+
+class TriangleViewController: UIViewController {
+    
+    @IBOutlet weak var baseInput: UITextField!
+    @IBOutlet weak var heightInput: UITextField!
+    @IBOutlet weak var result: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+    
+    
+}
+
